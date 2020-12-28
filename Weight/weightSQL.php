c938ec20c91dd36b40a67319df4e85eda189e0ac
Troy DeClerck <daeclerck@gmail.com>
2020-12-28 10:25:23 -0600

    conn fix

--- Weight/weightSQL.php
+++ Weight/weightSQL.php
@@ -25,7 +25,7 @@ function AddWeight($AccountID, $Date, $Unit, $Weight) {
 // Change existing weight to new weight
 function UpdateWeight($Weight, $AccountID, $Date) {
     $conn = connect();
-    $stmt = conn->prepare("UPDATE Weight SET CurrentWeight = ? WHERE FKAccountID = ? AND Recorded = ?");
+    $stmt = $conn->prepare("UPDATE Weight SET CurrentWeight = ? WHERE FKAccountID = ? AND Recorded = ?");
     $stmt->bind_param('dss', $Weight, $AccountID, $Date);
     $stmt->execute();
     $conn->close();
