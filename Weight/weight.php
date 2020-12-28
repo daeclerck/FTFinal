84b020336f5d876637ed2fc310bb63649cc6d281
Troy DeClerck <daeclerck@gmail.com>
2020-12-28 10:38:24 -0600

    GOTTA BE FIXED

--- Weight/weight.php
+++ Weight/weight.php
@@ -59,7 +59,7 @@
     if(isset($_POST['NewWeightSubmit']) && !empty($_POST['NewWeight'])) {
         $FetchDate = SelectDate($_POST['NewWeightSubmit'], $_SESSION['AccountID']);
 
-        UpdateWeight($_POST['NewWeightSubmit'], $_SESSION['AccountID'], $FetchDate['Recorded']);
+        UpdateWeight($_POST['NewWeight'], $_SESSION['AccountID'], $FetchDate['Recorded']);
         echo "Weight updated succesfully!";
         echo "<br>";
         PrintWeight($_SESSION['AccountID']);
