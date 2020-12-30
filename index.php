12c7557de87f904a64747dedbabc4aef62e4c236
Troy DeClerck <daeclerck@gmail.com>
2020-12-30 16:49:36 -0600

    edit to index

--- index.php
+++ index.php
@@ -19,9 +19,9 @@
 <!-- Header with full-height image -->
 <header class="bgimg-1 w3-display-container w3-grayscale-min" id="home">
   <div class="w3-display-left w3-text-white" style="padding:48px">
-    <span class="w3-jumbo w3-hide-small">Keep track of your health</span><br>
+    <span class="w3-jumbo w3-hide-small">Keep Track of Your Health</span><br>
     <span class="w3-xxlarge w3-hide-large w3-hide-medium">Keep track of your health</span><br>
-    <span class="w3-large">Track your calories, food intake, and exercise routines with this simple application.</span>
+    <span class="w3-large">Track your meals, calorie intake, and exercise routines with this simple application.</span>
     <p><a href="#about" class="w3-button w3-white w3-padding-large w3-large w3-margin-top w3-opacity w3-hover-opacity-off">Get started now!</a></p>
   </div> 
   <div class="w3-display-bottomleft w3-text-grey w3-large" style="padding:24px 48px">
