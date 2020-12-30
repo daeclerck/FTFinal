b87d58391e4e26f5fdba67e5d6d685334384b180
Troy DeClerck <daeclerck@gmail.com>
2020-12-30 08:43:53 -0600

    test image

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ body, html {
 .bgimg-1 {
   background-position: center;
   background-size: cover;
-  background-image: url("/w3images/mac.jpg");
+  background-image: url("/Fitness-Tracker-Home-Image.jpg");
   min-height: 100%;
 }
 
