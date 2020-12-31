6a7b3fc001e419a665cc5170d7dca0c141e993ef
Troy DeClerck <daeclerck@gmail.com>
2020-12-30 22:27:45 -0600

    added bold

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
   <div class="w3-display-left w3-text-black" style="padding:48px">
     <span class="w3-jumbo w3-hide-small">Keep Track of Your Health</span><br>
     <span class="w3-xxlarge w3-hide-large w3-hide-medium">Keep track of your health</span><br>
-    <span class="w3-large">Track your meals, calorie intake, and exercise routines with this simple application.</span>
+    <span class="w3-large"><b>Track your meals, calorie intake, and exercise routines with this simple application.<b></span>
     <p><a href="#about" class="w3-button w3-white w3-padding-large w3-large w3-margin-top w3-opacity w3-hover-opacity-off">Get started now!</a></p>
   </div> 
   <div class="w3-display-bottomleft w3-text-grey w3-large" style="padding:24px 48px">
