f9b845ff8cbb8ef8e38cac07727b7fb8abfc6f19
Troy DeClerck <daeclerck@gmail.com>
2020-12-30 22:37:13 -0600

    header asdd

--- User/user.php
+++ User/user.php
@@ -7,12 +7,14 @@
     if(!isset($_SESSION)) { session_start(); }
 ?>
     
-<form method="POST">  
+<form method="POST">
+<header class="bgimg-1 w3-display-container w3-grayscale-min">
     <h1>Add a new User</h1><br>
     <label>User Name: </label>
     <!-- Check for valid user names including no white spaces in beginning or end -->
     <input type="text" name="UserName" required pattern="^[-a-zA-Z0-9-()]+(\s+[-a-zA-Z0-9-()]+)*$" title="This field is required"><br>
     <input type="submit" name="UserNameSubmit" value="Add User">  
+</header>
 </form> 
 
 <?php
