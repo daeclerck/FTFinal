f0d37fb471e2e0fd9bc0d7eb987ab1294b1e178a
Troy DeClerck <daeclerck@gmail.com>
2021-01-01 14:45:06 -0600

    xxxlarge class

--- User/user.php
+++ User/user.php
@@ -9,7 +9,7 @@
 <br><br><br>
 <form method="POST">
     <!-- <h1>Add a new User</h1><br> -->
-    <span class="w3-large">Add a new User</span><br>
+    <span class="w3-xxxlarge">Add a new User</span><br>
     <label>User Name: </label>
     <!-- Check for valid user names including no white spaces in beginning or end -->
     <input type="text" name="UserName" required pattern="^[-a-zA-Z0-9-()]+(\s+[-a-zA-Z0-9-()]+)*$" title="This field is required"><br>
