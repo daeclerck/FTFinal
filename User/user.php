c193853fb81d3515ecaa0aebb4cf7c1128c37822
Troy DeClerck <daeclerck@gmail.com>
2021-01-01 16:28:04 -0600

    divs

--- User/user.php
+++ User/user.php
@@ -10,14 +10,14 @@
 <header class="bgimg-2">
 <div class="w3-container w3-light-grey w3-padding-64">
     <div class="w3-row-padding">
-        <div class="w3-col m6">
-<form method="POST">
-    <h1>Add a User</h1>
-    <label>User Name: </label>
-    <!-- Check for valid user names including no white spaces in beginning or end -->
-    <input type="text" name="UserName" required pattern="^[-a-zA-Z0-9-()]+(\s+[-a-zA-Z0-9-()]+)*$" title="This field is required">
-    <input type="submit" name="UserNameSubmit" value="Add User"> 
-</form> 
+        <div class="w3-col m4">
+            <form method="POST">
+                <h1>Add a User</h1>
+                <label>User Name: </label>
+                <!-- Check for valid user names including no white spaces in beginning or end -->
+                <input type="text" name="UserName" required pattern="^[-a-zA-Z0-9-()]+(\s+[-a-zA-Z0-9-()]+)*$" title="This field is required">
+                <input type="submit" name="UserNameSubmit" value="Add User"> 
+            </form> 
         </div>
 <?php
     if(isset($_POST['UserNameSubmit']) && !empty($_POST['UserName'])) {
@@ -31,20 +31,20 @@
     else { echo "Please enter a valid name."; }
 ?>  
 
-        <div class="w3-col m6">
-<form method="POST">   
-    <h1>Select a User</h1>
-    <select name="AccountID">
-    <option disabled selected value> -- select a user -- </option>
-    <?php
-        $account = SelectUser();
-        foreach($account as $result) {
-            echo "<option value='" . $result['AccountID'] . "'>" . $result['UserName'] . "</option>";
-        }    
-    ?>  
-    </select>
-    <input type="submit" name="ViewOptions" value="View Options"> 
-</form>    
+        <div class="w3-col m4">
+            <form method="POST">   
+                <h1>Select a User</h1>
+                <select name="AccountID">
+                <option disabled selected value> -- select a user -- </option>
+                <?php
+                    $account = SelectUser();
+                    foreach($account as $result) {
+                        echo "<option value='" . $result['AccountID'] . "'>" . $result['UserName'] . "</option>";
+                    }    
+                ?>  
+                </select>
+                <input type="submit" name="ViewOptions" value="View Options"> 
+            </form>    
         </div>
 <?php
     if(isset($_POST['ViewOptions']) && !empty($_POST['AccountID'])) {
@@ -57,21 +57,22 @@
         echo ConfirmUser($confirm) . " has been selected!";
     }  
 ?>
-<div class="w3-col m6">
-<form method="POST">
-    <h1>Delete a User</h1>
-    <select name="DeleteID">
-    <option disabled selected value> -- select a user -- </option>
-    <?php
-        $account = SelectUser();
-        foreach($account as $result) {
-            echo "<option value='" . $result['AccountID'] . "'>" . $result['UserName'] . "</option>";
-        }    
-    ?>  
-    </select>
-    <input type="submit" name="DeleteUser" value="Delete User">   
-</form>    
-</div>
+
+        <div class="w3-col m4">
+            <form method="POST">
+                <h1>Delete a User</h1>
+                <select name="DeleteID">
+                <option disabled selected value> -- select a user -- </option>
+                <?php
+                    $account = SelectUser();
+                    foreach($account as $result) {
+                        echo "<option value='" . $result['AccountID'] . "'>" . $result['UserName'] . "</option>";
+                    }    
+                ?>  
+                </select>
+                <input type="submit" name="DeleteUser" value="Delete User">   
+            </form>    
+        </div>
 <?php
     if(isset($_POST['DeleteUser']) && !empty($_POST['DeleteID'])) {
         // Remove the chosen AccountID and delete from database
