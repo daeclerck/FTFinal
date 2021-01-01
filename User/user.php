edb47c9a717562a97b52c7f5d37af5ada9f625d6
Troy DeClerck <daeclerck@gmail.com>
2021-01-01 16:34:02 -0600

    test

--- User/user.php
+++ User/user.php
@@ -10,7 +10,7 @@
 <header class="bgimg-2">
 <div class="w3-container w3-light-grey w3-padding-64">
     <div class="w3-row-padding">
-        <div class="w3-col m4">
+        <div class="w3-col m5">
             <form method="POST">
                 <h1>Add a User</h1>
                 <label>User Name: </label>
@@ -30,7 +30,7 @@
     }
 ?>  
 
-        <div class="w3-col m6">
+        <div class="w3-col m3">
             <form method="POST">   
                 <h1>Select a User</h1>
                 <select name="AccountID">
