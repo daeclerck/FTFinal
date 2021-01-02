444912ec91907937169218ff589fb5937916e5e3
Troy DeClerck <daeclerck@gmail.com>
2021-01-02 16:21:46 -0600

    syntax

--- Meal/meal.php
+++ Meal/meal.php
@@ -162,5 +162,5 @@
 </form>
 </header>
 </body>
-<?php echo "Session Account ID = " . $_SESSION['AccountID'] . "<br>Session User Name = " . $_SESSION['UserName'];
+<?php echo "Session Account ID = " . $_SESSION['AccountID'] . "<br>Session User Name = " . $_SESSION['UserName']; ?>
 </html>
