05c3afeae8914de3f2d2604fda7581da39465116
Troy DeClerck <daeclerck@gmail.com>
2021-01-02 17:35:27 -0600

    clarity

--- Food/food.php
+++ Food/food.php
@@ -31,76 +31,74 @@
                         'FatInput' => 0,
                         'CarbsInput' => 0,
                         'ProteinInput' => 0);
-    ?>
 
-<header class="bgimg-2">
-<?php
-if($_SERVER['REQUEST_METHOD'] == 'POST') {
-    // If the user submitted the form, process it
-	if(isset($_POST['NewFoodSubmit']) && !empty($_POST['FoodNameInput'])) {
+        if($_SERVER['REQUEST_METHOD'] == 'POST') {
+            // If the user submitted the form, process it
+	        if(isset($_POST['NewFoodSubmit']) && !empty($_POST['FoodNameInput'])) {
 
-            // Food Info
-            $FoodName = $_POST['FoodNameInput'];
-            $SizeUOM = $_POST['ServingSizeUOM'];
-            $ServingSize = $_POST['ServingSizeInput'];
-            $Calories = $_POST['CaloriesInput'];
-            $FoodID = RandomID();
+                // Food Info
+                $FoodName = $_POST['FoodNameInput'];
+                $SizeUOM = $_POST['ServingSizeUOM'];
+                $ServingSize = $_POST['ServingSizeInput'];
+                $Calories = $_POST['CaloriesInput'];
+                $FoodID = RandomID();
 
-            AddFood($FoodID, $FoodName, $SizeUOM, $ServingSize, $Calories);
+                AddFood($FoodID, $FoodName, $SizeUOM, $ServingSize, $Calories);
 
-            // Nutrient Info
-            $UOMname = "Grams"; // Hard coded as grams
+                // Nutrient Info
+                $UOMname = "Grams"; // Hard coded as grams
 
-            $MacroArray = array();
-            $MacroArray['Fat'] = $_POST['FatInput'];
-            $MacroArray['Carbohydrates'] = $_POST['CarbsInput'];
-            $MacroArray['Protein'] = $_POST['ProteinInput'];
+                $MacroArray = array();
+                $MacroArray['Fat'] = $_POST['FatInput'];
+                $MacroArray['Carbohydrates'] = $_POST['CarbsInput'];
+                $MacroArray['Protein'] = $_POST['ProteinInput'];
 
-            // Insert amounts into Food Nutrient table
-            foreach($MacroArray as $key => $value) {
-                AddNutrients($key, $FoodID, $SizeUOM, $value);
-            }
-
-            foreach($_SESSION["NutrientValueArray"] as $key => $value) {
-                // Only commit micros if they have a value above zero
-                if($value > 0) {
+                // Insert amounts into Food Nutrient table
+                foreach($MacroArray as $key => $value) {
                     AddNutrients($key, $FoodID, $SizeUOM, $value);
                 }
-	        }   
 
-	    // Reset the micronutrient array
-        $MicroReset = SelectMicro();
-        foreach($MicroReset as $key) {
-            $_SESSION["NutrientValueArray"][$key["NutrientName"]] = 0;
-	    }	    
-	    
-    }
+                foreach($_SESSION["NutrientValueArray"] as $key => $value) {
+                    // Only commit micros if they have a value above zero
+                    if($value > 0) {
+                        AddNutrients($key, $FoodID, $SizeUOM, $value);
+                    }
+                }   
 
-	else if(isset($_POST['MicroSubmit']) && !empty($_POST['MicroSelect'])) {
-	    // Retain the values in the form
-	    $FoodInfo['FoodNameInput'] = $_POST['FoodNameInput'];
-	    $FoodInfo['CaloriesInput'] = $_POST['CaloriesInput'];
-	    $FoodInfo['ServingSizeInput'] = $_POST['ServingSizeInput'];
-	    $FoodInfo['FatInput'] = $_POST['FatInput'];
-	    $FoodInfo['CarbsInput'] = $_POST['CarbsInput'];
-	    $FoodInfo['ProteinInput'] = $_POST['ProteinInput'];
-	    
-        $key = $_POST['MicroSelect'];
-        $value = $_POST['MicroInput'];
+                // Reset the micronutrient array
+                $MicroReset = SelectMicro();
+                foreach($MicroReset as $key) {
+                    $_SESSION["NutrientValueArray"][$key["NutrientName"]] = 0;
+                }	    
+                    
+            }
 
-	    $_SESSION["NutrientValueArray"]["$key"] = $value;
-    }
+	        else if(isset($_POST['MicroSubmit']) && !empty($_POST['MicroSelect'])) {
+                // Retain the values in the form
+                $FoodInfo['FoodNameInput'] = $_POST['FoodNameInput'];
+                $FoodInfo['CaloriesInput'] = $_POST['CaloriesInput'];
+                $FoodInfo['ServingSizeInput'] = $_POST['ServingSizeInput'];
+                $FoodInfo['FatInput'] = $_POST['FatInput'];
+                $FoodInfo['CarbsInput'] = $_POST['CarbsInput'];
+                $FoodInfo['ProteinInput'] = $_POST['ProteinInput'];
+                
+                $key = $_POST['MicroSelect'];
+                $value = $_POST['MicroInput'];
+
+                $_SESSION["NutrientValueArray"]["$key"] = $value;
+            }
     
-    else if(isset($_POST['DeleteFoodSubmit'])) {
-        
-        if(DeleteFoodConfirm($_POST['DeleteFood'])) {
-            DeleteFood($_POST['DeleteFood']);
-            echo "Food Deleted Successfully.";
+            else if(isset($_POST['DeleteFoodSubmit'])) {
+                if(DeleteFoodConfirm($_POST['DeleteFood'])) {
+                    DeleteFood($_POST['DeleteFood']);
+                    echo "Food Deleted Successfully.";
+                }
+                else { $AlreadyStored = True; }
+            }
         }
-        else { $AlreadyStored = True; }
-    }
-}
-?>
+    ?>
+    
+<header class="bgimg-2">
 <h1 class="w3-center title"><b><em>Add Your Favorite Foods Here</em></b></h1>
 <div class="w3-container" style="Padding:16px 64px">
 
