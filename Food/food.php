0c25ddc74bda2a1d93c40422c08a1baf15ff865b
Troy DeClerck <daeclerck@gmail.com>
2021-01-02 11:57:50 -0600

    aa

--- Food/food.php
+++ Food/food.php
@@ -107,9 +107,9 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 <div class="w3-col m6"> 
 <form method="POST">
-    <h6><b>Food Name</b></h6>
+    <label><b>Food Name</b></label>
     <!-- Check for valid food names including no white spaces in beginning or end -->
-    <input class="w3-animate-input" name="FoodNameInput" type="text" value="<?php echo $FoodInfo['FoodNameInput']; ?>" pattern="^[-a-zA-Z0-9-()]+(\s+[-a-zA-Z0-9-()]+)*$" title="This field is required">
+    <input name="FoodNameInput" type="text" value="<?php echo $FoodInfo['FoodNameInput']; ?>" pattern="^[-a-zA-Z0-9-()]+(\s+[-a-zA-Z0-9-()]+)*$" title="This field is required">
     <br>
     <label>Serving Size: </label>
     <input name="ServingSizeInput" type="number" value="<?php echo $FoodInfo['ServingSizeInput']; ?>" min="0" max="999.99" step="0.01">
