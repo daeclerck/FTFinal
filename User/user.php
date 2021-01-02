f6bbc04f22e8cfa6aafad337129a5a1e71efcbdc
Troy DeClerck <daeclerck@gmail.com>
2021-01-02 16:10:11 -0600

    adding comments to user

--- User/user.php
+++ User/user.php
@@ -1,27 +1,32 @@
 <html>
 <body>
-<?php 
-    include "userSQL.php";
-    include "../header.php";
+    <?php
+        // Include SQL methods and header
+        include "userSQL.php";
+        include "../header.php";
 
-    $DeleteSuccess = False;
- 
-    if(!isset($_SESSION)) { session_start(); }
+        // Initialize necessary variables
+        $DeleteSuccess = False;
 
-    if(isset($_POST['DeleteUser']) && !empty($_POST['DeleteID'])) {
-        // Remove the chosen AccountID and delete from database
-	    $delete = $_POST['DeleteID'];
-        DeleteUser($delete);
-        $DeleteSuccess = True;	
-    } 
-?>
+        // Begin session
+        if(!isset($_SESSION)) { session_start(); }
+
+        // User submits Delete User
+        if(isset($_POST['DeleteUser']) && !empty($_POST['DeleteID'])) {
+            // Remove the chosen AccountID and delete from database
+            $delete = $_POST['DeleteID'];
+            DeleteUser($delete);
+            $DeleteSuccess = True;	
+        } 
+    ?>
+
 <header class="bgimg-2">
 <h1 class="w3-center" style="Padding:80px"><b><em>Begin tracking your fitness today</em></b></h1>
 <div class="w3-container" style="Padding:0px 32px">
     <div class="w3-row-padding">
         <div class="w3-col m5">
             <form method="POST">
-                <h1><i class="fa fa-plus w3-xxlarge"></i> Add a User</h1>
+                <h1><i class="fa fa-plus w3-xxlarge"></i>Add a User</h1>
                 <!-- Check for valid user names including no white spaces in beginning or end -->
                 <input type="text" name="UserName" required pattern="^[-a-zA-Z0-9-()]+(\s+[-a-zA-Z0-9-()]+)*$" title="This field is required" placeholder="Enter Valid Name">
                 <input type="submit" name="UserNameSubmit" value="Add User"> 
