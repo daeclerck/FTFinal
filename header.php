122c8f0da958ff291faf616c949fc38d12b788e7
Troy DeClerck <daeclerck@gmail.com>
2021-01-02 22:31:53 -0600

    css

--- header.php
+++ header.php
@@ -43,6 +43,29 @@ select {
 .title {
   margin-top: 80px;
 }
+
+/* Table style */
+#table {
+  border-collapse: collapse;
+  width: 100%;
+}
+
+#table td, #table th {
+  border: 1px solid #ddd;
+  padding: 8px;
+}
+
+#table tr:nth-child(even){background-color: #f2f2f2;}
+
+#table tr:hover {background-color: #ddd;}
+
+#table th {
+  padding-top: 12px;
+  padding-bottom: 12px;
+  text-align: left;
+  background-color: #000000;
+  color: white;
+}
 </style>
 
 <!-- Navbar (sit on top) -->
