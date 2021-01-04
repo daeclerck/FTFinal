5b422d125c1d395bc2b1f2aa16c455f9c0edfd55
Troy DeClerck <daeclerck@gmail.com>
2021-01-04 11:44:33 -0600

    instructions

--- Food/foodINFO.php
+++ Food/foodINFO.php
@@ -78,8 +78,10 @@
 <h1 class="w3-center title"><b><em>Find Nutrient Information For Registered Food</em></b></h1>
 <form method="POST">
     <div class="w3-container" style="Padding:16px 64px">
-        <div class="w3-col m4"> 
-        <label><b>Food Name </b></label>
+        <div class="w3-col m4">
+        <h6><em>Search for a food to fetch the nutrient information.</em></h6>
+        <br><h6><b>NOTE:</b><em> Leaving input blank will show the entire selection.</em></h6>
+        <br><label><b>Food Name </b></label>
         <input name="SearchFoodInput" type="text" pattern="^[-a-zA-Z0-9-()]+(\s+[-a-zA-Z0-9-()]+)*$">
         <input name="SearchFoodSubmit" type="submit" value="Search for Food">
         <br><select name="SearchFoodResults">
