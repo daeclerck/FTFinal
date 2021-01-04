0c8804b717c498440bfd03107afa1cf9d93ce6a3
Troy DeClerck <daeclerck@gmail.com>
2021-01-04 12:52:40 -0600

    css

--- Meal/meal.php
+++ Meal/meal.php
@@ -106,7 +106,7 @@
     <br>
     <label>Search Results: </label>
     <br>
-    <select name="SearchFoodResult" style="height:75%;width40%">
+    <select name="SearchFoodResult" style="height:15%; width40%;">
         <?php
             if(isset($_POST['SearchFoodSubmit']) || isset($_POST['AddFoodToMealSubmit'])) {
                 $FoodResult = SelectFood($FoodSearch);
