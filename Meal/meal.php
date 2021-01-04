7d836e228d34bbdf9db85d1009be4edf92091e54
Troy DeClerck <daeclerck@gmail.com>
2021-01-04 14:12:55 -0600

    css

--- Meal/meal.php
+++ Meal/meal.php
@@ -159,7 +159,7 @@
     </select>
     <br><input name="RemoveFoodSubmit" type="submit" value="Remove From Meal">
     <input name="ClearMealSubmit" type="submit" value="Clear Meal">
-    <input class="w3-button w3-black" name="AddMealSubmit" type="submit" value="Add This Meal">
+    <br><input class="w3-button w3-black" name="AddMealSubmit" type="submit" value="Add This Meal">
 </div>
 
 
