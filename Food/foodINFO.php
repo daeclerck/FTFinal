5a05ef25c07eb6bed9a0a58e07343c87a2911fc4
Troy DeClerck <daeclerck@gmail.com>
2021-01-03 18:15:39 -0600

    css

--- Food/foodINFO.php
+++ Food/foodINFO.php
@@ -78,7 +78,7 @@
 <h1 class="w3-center title"><b><em>Find Nutrient Information For Registered Food</em></b></h1>
 <form method="POST">
     <div class="w3-container" style="Padding:16px 64px">
-        <div class="w3-quarter w3-right-align" style="margin-right:16px"> 
+        <div class="w3-col 4"> 
         <label><b>Food Name </b></label>
         <input name="SearchFoodInput" type="text" pattern="^[-a-zA-Z0-9-()]+(\s+[-a-zA-Z0-9-()]+)*$">
         <input name="SearchFoodSubmit" type="submit" value="Search for Food">
@@ -123,7 +123,7 @@
         }
     ?>
     </div>
-    <div class="w3-quarter">
+    <div class="w3-col 4">
     <label>Name </label>
     <select name="FoodDisplay" size="1" disabled>
         <option value="<?php echo $FoodID; ?>">
@@ -150,7 +150,7 @@
     <input disabled type="text" value="<?php echo $Calories; ?>">
     </div>
 
-    <div class="w3-quarter" style="margin-left:16px">
+    <div class="w3-col 4">
     <table>
         <?php
             if(!empty($FoodID)) {
