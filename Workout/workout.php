edf7385d2cfa26de37b6de2942e186ac01b7a40a
Troy DeClerck <daeclerck@gmail.com>
2021-01-06 13:14:36 -0600

    errortest

--- Workout/workout.php
+++ Workout/workout.php
@@ -11,9 +11,8 @@
     if(empty($_SESSION['AccountID']) || empty($_SESSION['UserName'])) {
         // A user needs to be selected first
         //header('Location: ../User/user.php');
-        $message = echo "<h5 class='w3-center'><b>Select a user!</b><h5>";
-        header('Refresh: 5; url=../User/user.php');
-        exit($message);
+        echo "<h5 class='w3-center'><b>Select a user!</b><h5>";
+        exit();
     }
 
     $CurrentUser = $_SESSION['AccountID'];
