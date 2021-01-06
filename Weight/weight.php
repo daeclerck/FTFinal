569ca5dd5685c02179c641557d5f0994657865f1
Troy DeClerck <daeclerck@gmail.com>
2021-01-06 10:42:32 -0600

    formatting

--- Weight/weight.php
+++ Weight/weight.php
@@ -1,32 +1,34 @@
-<?php include "../header.php"; ?>
 <html>
 <body>
+
+    <?php
+        // Include SQL methods and header
+        include "weightSQL.php";
+        include "../header.php";
+        
+        // Begin session
+        if(!isset($_SESSION)) { session_start(); }
+
+        // Send to user page if not selected
+        if(empty($_SESSION['AccountID']) || empty($_SESSION['UserName'])) {
+            // A user needs to be selected first
+            header('Location: ../User/user.php');
+        }
+
+        if(isset($_POST['NewWeightSubmit']) && !empty($_POST['NewWeight']) && is_numeric($_POST['NewWeight'])) {
+            // Fetch the correct date to corresponding weight
+            $FetchDate = SelectDate($_POST['WeightChange'], $_SESSION['AccountID']);
+
+            // Update the database with the new weight and send success message
+            UpdateWeight($_POST['NewWeight'], $_SESSION['AccountID'], $FetchDate['Recorded']);
+        }
+    ?>
+
 <header class="bgimg-2">
-<?php 
-    include "weightSQL.php"; 
-    
-    if(!isset($_SESSION)) { session_start(); }
-
-    if(empty($_SESSION['AccountID']) || empty($_SESSION['UserName'])) {
-        // A user needs to be selected first
-        header('Location: ../User/user.php');
-        //exit("Sorry, the current session has expired. Please log in again.");
-    }
-?>
-
-<?php
-    if(isset($_POST['NewWeightSubmit']) && !empty($_POST['NewWeight']) && is_numeric($_POST['NewWeight'])) {
-        // Fetch the correct date to corresponding weight
-        $FetchDate = SelectDate($_POST['WeightChange'], $_SESSION['AccountID']);
-
-        // Update the database with the new weight and send success message
-        UpdateWeight($_POST['NewWeight'], $_SESSION['AccountID'], $FetchDate['Recorded']);
-    }
-?>
-
+<h1 class="w3-center title"><b><em>Add Your Current Weight or Change Previous Entries</em></b></h1>
 <form method="POST">
     <h1>Update Weight</h1>
-    <label>New Weight: </label>
+    <label>New Weight </label>
     <input type="text" name="Weight">
     <label>Unit of Measurement: </label>
     <select name="UnitMeasure">
@@ -36,19 +38,20 @@
     <input type="submit" name="WeightSubmit" value="Submit Weight">
 </form>
 
-<?php
-    if(isset($_POST['WeightSubmit']) && !empty($_POST['Weight']) && is_numeric($_POST['Weight'])) {
-        $CurrentDate = date("Y-m-d H:i:s");
-        AddWeight($_SESSION['AccountID'], $CurrentDate, $_POST['UnitMeasure'], $_POST['Weight']);
-        echo "Weight added to " . $_SESSION['UserName'] . "'s account<br>";
-        PrintWeight($_SESSION['AccountID']);
-    } 
-?>
+    <?php
+        if(isset($_POST['WeightSubmit']) && !empty($_POST['Weight']) && is_numeric($_POST['Weight'])) {
+            $CurrentDate = date("Y-m-d H:i:s");
+            AddWeight($_SESSION['AccountID'], $CurrentDate, $_POST['UnitMeasure'], $_POST['Weight']);
+            echo "Weight added to " . $_SESSION['UserName'] . "'s account<br>";
+            PrintWeight($_SESSION['AccountID']);
+        } 
+    ?>
 
-<form method="POST" action="">
+<form method="POST">
     <h1>Change Previous Weight</h1>
     <label>Choose the weight to change: </label>
     <select name="WeightChange">
+
         <?php
             $WeightResult = SelectWeight($_SESSION['AccountID']);
 
@@ -58,21 +61,21 @@
                 echo "</option>";
             }
         ?>
+
     </select>
-    <br>
-    <label>Change selected weight to: </label>
+    <br><label>Change selected weight to: </label>
     <input type="text" name="NewWeight">
-    <br>
-    <input type="submit" name="NewWeightSubmit" value="Submit New Weight">
+    <br><input type="submit" name="NewWeightSubmit" value="Submit New Weight">
 </form>
 
-<?php
-    if(isset($_POST['NewWeightSubmit']) && !empty($_POST['NewWeight']) && is_numeric($_POST['NewWeight'])) {
-        echo "Weight updated succesfully!";
-        echo "<br>";
-        PrintWeight($_SESSION['AccountID']);
-    } 
-?>
+    <?php
+        if(isset($_POST['NewWeightSubmit']) && !empty($_POST['NewWeight']) && is_numeric($_POST['NewWeight'])) {
+            echo "Weight updated succesfully!";
+            echo "<br>";
+            PrintWeight($_SESSION['AccountID']);
+        } 
+    ?>
+    
 </header>
 </body>
 </html>
